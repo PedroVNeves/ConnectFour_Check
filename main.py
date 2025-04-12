10ef9c2083306c46282f6113a66fa2884253720c
Pedro Neves <pedro.vasconcelosneves@gmail.com>
2025-04-12 17:34:16 -0300

    main.py atualization

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-class ConnectFour():
-    def __init__(self,board: list[list[str]]): # receive a list of lists that represents the board
+class ConnectFour:
+    def __init__(self, board: list[list[str]]):
         self.board = board
         self.num_x = 0
         self.num_o = 0
         self.rows = len(board)
         self.cols = len(board[0])
         self.x_wins = 0
-        self.y_winx = 0
+        self.o_wins = 0
         
     def verify(self):
-        if self.verify_symbols and self.verify_empty_space and  self.verify_num_of_symbols:
-            return self.verify_winner
+        if self.verify_symbols() and self.verify_empty_space() and self.verify_num_of_symbols():
+            return self.verify_winner()
         else:
             return 'INVALID'   
-        
-        
     
     def verify_symbols(self):
         for line in self.board:
             for symbol in line:
-                if not symbol.lower() in 'xo.':
+                if symbol.lower() not in 'xo.':
                     return False
+        return True
     
     def verify_empty_space(self):
-        for i in range(self.rows -1):
+        for i in range(self.rows - 1):
             for j in range(self.cols):
-             if self.board[i][j].lower() != '.' and self.board[i+1][j].lower == '.':
-                 return False
+                if self.board[i][j].lower() != '.' and self.board[i+1][j].lower() == '.':
+                    return False
+        return True
 
     def verify_num_of_symbols(self):
         for line in self.board:
@@ -35,11 +35,9 @@ class ConnectFour():
                     self.num_x += 1 
                 elif symbol.lower() == 'o':
                     self.num_o += 1
-                else:
-                    continue
-                
-                
-    def verify_winner(self) -> str | None:
+        return abs(self.num_x - self.num_o) <= 1
+
+    def verify_winner(self) -> str:
         directions = [(-1, -1), (-1, 0), (-1, 1),
                       (0,  -1),          (0, 1),
                       (1,  -1), (1, 0),  (1, 1)]
@@ -49,32 +47,31 @@ class ConnectFour():
                 symbol = self.board[i][j].lower()
                 if symbol == '.':
                     continue
-                for direction_x, direction_y in directions:
+                for dx, dy in directions:
                     count = 1
                     x, y = i, j
                     for _ in range(3):
-                        x += direction_x
-                        y += direction_y
-                        if 0 <= x <= self.rows and 0 <= y <= self.cols and self.board[x][y].lower() == symbol:
+                        x += dx
+                        y += dy
+                        if 0 <= x < self.rows and 0 <= y < self.cols and self.board[x][y].lower() == symbol:
                             count += 1
                         else:
                             break
                     if count == 4:
-                        if symbol.lower == 'x':
-                            self.x_wins =+ 1       #It will count the outward and return journeys twice,
-                        if symbol.lower == 'o':    #but this will not influence the final result.
-                            self.o_wins =+ 1
+                        if symbol == 'x':
+                            self.x_wins += 1
+                        elif symbol == 'o':
+                            self.o_wins += 1
         
-        if self.x_wins == self.o_wins > 0:
+        if self.x_wins > 0 and self.o_wins > 0:
             return 'INVALID'
-        
-        elif self.x_wins > self.o_wins and self.num_x > self.num_o:
+        elif self.x_wins > 0:
             return 'VALID X'
-        elif self.x_wins < self.o_wins:
-            return 'VALID O'        
+        elif self.o_wins > 0:
+            return 'VALID O'
         else:
             return 'VALID'
-    
+
 
 final_board = []
 for i in range(6):
@@ -86,5 +83,3 @@ print(board.verify())
 
 
 
-        
-            
